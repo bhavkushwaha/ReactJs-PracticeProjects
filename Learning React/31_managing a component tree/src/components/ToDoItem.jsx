becaf1e84e27a6b56a2eae23fca0efd3f3daa252
Bhav Kushwaha <75977991+bhavkushwaha@users.noreply.github.com>
2023-01-25 01:04:25 +0530

    Add files via upload

---
+++ Learning React/31_managing a component tree/src/components/ToDoItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ToDoItem(props) {
+  return (
+    <div onClick={props.onChecked}>
+      <li>{props.text}</li>;
+    </div>
+  );
+}
+
+export default ToDoItem;
